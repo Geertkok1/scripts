95aa6e67033a9f7e5997ac180d7ded893f53851f
Geert Kok <geertkok96@gmail.com>
2018-07-19 19:10:44 +0200

    last minute changes

--- web/index.php
+++ web/index.php
@@ -1,7 +1,11 @@
 <?php
 echo "Ping test tool";
 echo "<form action="index.php">
-Hostname or IP to ping <input type="text" name="host"></input>
+Hostname or IP to ping <input type="text" name="host" id="host"></input>
 <input type="submit" value="Submit">
 </form>";
+$host = $_GET['host']
+$command = escapeshellcmd('python /usr/pi/checkifup.py \'$host\' ');
+$output = shell_exec($command);
+echo $output
  ?>
