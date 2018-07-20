5505c6c61a9df666b2da66f4c4f22d0e29270eb0
Geert Kok <geertkok96@gmail.com>
2018-07-20 14:26:06 +0200

    change script location

--- web/index.php
+++ web/index.php
@@ -5,7 +5,7 @@ Hostname or IP to ping <input type="text" name="host" id="host"></input>
 <input type="submit" value="Submit">
 </form>";
 $host = $_GET['host']
-$command = escapeshellcmd('python /usr/pi/checkifup.py \'$host\' ');
+$command = escapeshellcmd('python /var/www/html/checkifup.py \'$host\' ');
 $output = shell_exec($command);
 echo $output
  ?>
