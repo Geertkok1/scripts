20805e0422f930a40e20785f5fdbd8ff0a3b35a3
Geert Kok <geertkok96@gmail.com>
2018-07-20 15:26:44 +0200

    add isset for checking if from was filled in

--- web/index.php
+++ web/index.php
@@ -5,8 +5,8 @@ echo "<form action=\"index.php\">
 Hostname or IP to ping <input type=\"text\" name=\"host\" id=\"host\"></input>
 <input type=\"submit\" value=\"Submit\">
 </form>";
-$host = $_GET['host'];
+if(isset($_GET['host'])){$host = $_GET['host'];
 $command = escapeshellcmd('python /var/www/html/scripts/web/checkifup.py '.$host);
 $output = shell_exec($command);
-echo $output;
+echo $output;}
  ?>
