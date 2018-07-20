8a37d81066a61e9f2bdd8fd8a014668039a189d2
Geert Kok <geertkok96@gmail.com>
2018-07-20 15:20:44 +0200

    add .

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@ Hostname or IP to ping <input type=\"text\" name=\"host\" id=\"host\"></input>
 <input type=\"submit\" value=\"Submit\">
 </form>";
 $host = $_GET['host'];
-$command = escapeshellcmd('python /var/www/html/scripts/web/checkifup.py '$host);
+$command = escapeshellcmd('python /var/www/html/scripts/web/checkifup.py '.$host);
 $output = shell_exec($command);
 echo $output;
  ?>
