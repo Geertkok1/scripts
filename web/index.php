d9535979dc514e198ccc96e4903890e4731807ca
Geert Kok <geertkok96@gmail.com>
2018-07-20 15:00:48 +0200

    add semicolon

--- web/index.php
+++ web/index.php
@@ -4,8 +4,8 @@ echo "<form action="index.php">
 Hostname or IP to ping <input type="text" name="host" id="host"></input>
 <input type="submit" value="Submit">
 </form>";
-$host = $_GET['host']
+$host = $_GET['host'];
 $command = escapeshellcmd('python /var/www/html/checkifup.py \'$host\' ');
 $output = shell_exec($command);
-echo $output
+echo $output;
  ?>
