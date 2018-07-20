5b11bf8dff29262117d2ac06ace020a25536bd1e
Geert Kok <geertkok96@gmail.com>
2018-07-20 15:14:13 +0200

    escape a few quotes

--- web/index.php
+++ web/index.php
@@ -1,9 +1,9 @@
 <?php
 ini_set('display_errors', 1);
 echo "Ping test tool";
-echo "<form action="index.php">
-Hostname or IP to ping <input type="text" name="host" id="host"></input>
-<input type="submit" value="Submit">
+echo "<form action=\"index.php\">
+Hostname or IP to ping <input type=\"text\" name=\"host\" id=\"host\"></input>
+<input type=\"submit\" value=\"Submit\">
 </form>";
 $host = $_GET['host'];
 $command = escapeshellcmd('python /var/www/html/scripts/web/checkifup.py 192.168.1.1 ');
